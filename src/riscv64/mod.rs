69c3cb751cda32e7dfff4e213cc123b261647ede
yufeng <321353225@qq.com>
2024-07-24 10:18:39 +0800

    feat: add doc and test script

--- src/riscv64/mod.rs
+++ src/riscv64/mod.rs
@@ -201,6 +201,8 @@ impl MultiCore {
     }
 }
 
+/// PolyHAL defined percpu reserved fields.
+/// Just used in the polyHAL and context switch.
 #[repr(C)]
 pub(crate) struct PerCPUReserved {
     pub user_rsp: usize,
@@ -208,6 +210,13 @@ pub(crate) struct PerCPUReserved {
     pub user_context: usize,
 }
 
+/// Get the offset of the specified percpu field.
+/// 
+/// PerCPU Arrange is that.
+/// 
+/// IN x86_64. The Reserved fields was used in manually.
+/// IN other architectures, the reserved fields was defined 
+/// negative offset of the percpu pointer.
 pub macro PerCPUReservedOffset($field: ident) {
     core::mem::offset_of!(PerCPUReserved, $field) as isize
         - core::mem::size_of::<PerCPUReserved>() as isize
